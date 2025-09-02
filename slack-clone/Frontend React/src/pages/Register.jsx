0793d1a570de908fdaf57d44091e311603415c7a
farhan basha <147364590+Farhan-basha@users.noreply.github.com>
2025-09-02 07:38:05 +0530

    update register.jsx

--- slack-clone/Frontend React/src/pages/Register.jsx
+++ slack-clone/Frontend React/src/pages/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8000/api/register/', {
+      const response = await fetch('https://backend-7tz9.onrender.com/api/register/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password }),
